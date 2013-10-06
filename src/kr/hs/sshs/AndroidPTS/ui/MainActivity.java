e3ed2317f9e23a8463b5732f5f61585e4e240e67
stephen422 <stephen422@gmail.com>
2013-10-06 17:04:04 +0900

    Fix library missing error & Update JavaCV version to 0.6

--- src/kr/hs/sshs/AndroidPTS/ui/MainActivity.java
+++ src/kr/hs/sshs/AndroidPTS/ui/MainActivity.java
@@ -112,7 +112,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 							mh.sendMessage(mh.obtainMessage(100));	// When done
 						} catch (java.lang.Exception e) {
 							e.printStackTrace();
-							Log.e("STOP", e.getMessage());
+							//Log.e("STOP", e.getMessage());
 						}
 					}
 				}.start();
